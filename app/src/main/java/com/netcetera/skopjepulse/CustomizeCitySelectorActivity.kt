fc03f8ddc3e15c62190971c6e8c34878392af534
Metodi Nakov <Metodi.Nakov@netcetera.com>
2020-06-16 12:27:58 +0200

    Adding customize city selector

---
+++ app/src/main/java/com/netcetera/skopjepulse/CustomizeCitySelectorActivity.kt
@@ -0,0 +1,12 @@
+package com.netcetera.skopjepulse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomizeCitySelectorActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_customize_city_selector)
+  }
+}
--- app/src/main/java/com/netcetera/skopjepulse/cityselect/CitySelectFragment.kt
+++ app/src/main/java/com/netcetera/skopjepulse/cityselect/CitySelectFragment.kt
@@ -1,25 +1,32 @@
 package com.netcetera.skopjepulse.cityselect
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.netcetera.skopjepulse.CustomizeCitySelectorActivity
 import com.netcetera.skopjepulse.R
 import com.netcetera.skopjepulse.base.BaseFragment
 import com.netcetera.skopjepulse.main.MainViewModel
-import kotlinx.android.synthetic.main.city_select_fragment_layout.citySelectRecyclerView
-import kotlinx.android.synthetic.main.city_select_fragment_layout.citySelectRefreshView
+import kotlinx.android.synthetic.main.city_select_fragment_layout.*
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+
 
 class CitySelectFragment : BaseFragment<CitySelectViewModel>() {
   override val viewModel: CitySelectViewModel by viewModel()
   private val mainViewModel : MainViewModel by sharedViewModel()
+  private lateinit var fabButton: FloatingActionButton
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     return inflater.inflate(R.layout.city_select_fragment_layout, container, false)
@@ -38,6 +45,24 @@ class CitySelectFragment : BaseFragment<CitySelectViewModel>() {
       mainViewModel.showForCity(it)
       parentFragmentManager.popBackStack()
     }
+
+    fabButton = fab
+    fabButton.setOnClickListener{
+      val intent = Intent(activity, CustomizeCitySelectorActivity::class.java)
+      startActivity(intent)
+    }
+
+    val scrollListener = object : RecyclerView.OnScrollListener() {
+      override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        when (newState) {
+          RecyclerView.SCROLL_STATE_IDLE -> fab.show()
+          else -> fab.hide()
+        }
+        super.onScrollStateChanged(recyclerView, newState)
+      }
+    }
+    citySelectRecyclerView.clearOnScrollListeners()
+    citySelectRecyclerView.addOnScrollListener(scrollListener)
 
     citySelectRefreshView.setOnRefreshListener {
       viewModel.refreshData(true)
