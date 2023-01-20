e4ed7dfe723523018863b679e17dcf01738b728b
Filip Mihajlov <47906006+filipmihajlov@users.noreply.github.com>
2023-01-20 13:42:11 +0100

    update versionCode and versionName for release

--- buildSrc/src/main/java/ProjectProperties.kt
+++ buildSrc/src/main/java/ProjectProperties.kt
@@ -6,6 +6,6 @@ object ProjectProperties {
 
 object PulseEco {
   const val applicationId = "com.netcetera.skopjepulse"
-  const val versionCode = 35
-  const val versionName = "4.2.1-22122022"
+  const val versionCode = 36
+  const val versionName = "4.2.2-20012023"
 }
